75dd9767db8ef0f311458d1b8b64265c3f00bb96
ColdFireHunter <53087752+ColdFireHunter@users.noreply.github.com>
2022-06-12 20:23:32 +0200

    added libary

---
+++ pins/pins.h
@@ -0,0 +1,19 @@
+#ifndef _pins_h_
+#define _pins_h_
+#include <Arduino.h>
+
+//LEDs
+#define TEST_LED PB0
+#define SDB PA8
+
+//ANALOG
+#define V_BAT_SENSE PA0
+#define ANTENNA PA1
+
+//BUTTONS
+#define BUTTON_1 PA2
+#define BUTTON_2 PA3
+#define DIFFICULTY_BUTTON PA6
+#define PLAYER_BUTTON PA7
+
+#endif
